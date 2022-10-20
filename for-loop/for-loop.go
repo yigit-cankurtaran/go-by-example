744e84c68afb80af5b14773656d63ffa11849d09
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-20 11:58:32 +0300

    Fixed formatting.

--- for-loop/for-loop.go
+++ for-loop/for-loop.go
@@ -8,22 +8,13 @@ func main() {
     fmt.Println(i)
     i += 1
   }
-
-  fmt.Println("\n")
-
   for j := 7; j <= 9; j++ {
     fmt.Println(j)
   }
-
-  fmt.Println("\n")
-
   for {
     fmt.Println("loopy")
     break
   }
-
-  fmt.Println("\n")
-
   for n := 0; n <= 5; n++ {
     if n % 2 == 0{
       continue
